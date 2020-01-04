c93c1e23b5fa2f06137d42a206bd3f522b78dd18
Zoe Ames <zoeames@gmail.com>
2020-01-04 12:41:30 -0600

    firebase app initialized

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import './App.scss';
 
+import firebaseConnection from '../helpers/data/connection';
 import Home from '../components/pages/Home/Home';
 import Auth from '../components/pages/Auth/Auth';
 import SingleBoard from '../components/pages/SingleBoard/SingleBoard';
@@ -17,10 +21,27 @@ const PrivateRoute = ({ component: Component, authed, ...rest }) => {
   return <Route {...rest} render={(props) => routeChecker(props)} />;
 };
 
+firebaseConnection();
+
 class App extends React.Component {
   state = {
-    authed: true,
+    authed: false,
   };
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        this.setState({ authed: true});
+      } else {
+        this.setState({ authed: false});
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
     const {authed} = this.state;
     return (
