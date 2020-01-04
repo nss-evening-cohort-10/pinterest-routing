6b429a919124cb87174bfaf447c49909fb8e5b6a
Zoe Ames <zoeames@gmail.com>
2020-01-04 11:01:08 -0600

    added routes

---
+++ src/components/pages/SingleBoard/SingleBoard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './SingleBoard.scss';
+
+class SingleBoard extends React.Component {
+  render() {
+    return (
+      <div className="SingleBoard">
+        <h1>Single Board</h1>
+      </div>
+    );
+  }
+}
+
+export default SingleBoard;
