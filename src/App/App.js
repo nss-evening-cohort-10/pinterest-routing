2967f9c7bdd8ed03a97a0232955c6f69471184c0
Zoe Ames <zoeames@gmail.com>
2020-01-04 13:10:58 -0600

    navbar wired up

--- src/App/App.js
+++ src/App/App.js
@@ -6,6 +6,8 @@ import 'firebase/auth';
 import './App.scss';
 
 import firebaseConnection from '../helpers/data/connection';
+import MyNavbar from '../components/shared/MyNavbar/MyNavbar'
+
 import Home from '../components/pages/Home/Home';
 import Auth from '../components/pages/Auth/Auth';
 import SingleBoard from '../components/pages/SingleBoard/SingleBoard';
@@ -47,6 +49,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Router>
+          <MyNavbar authed={authed}/>
           <Switch>
             <PrivateRoute path="/" exact component={Home} authed={authed}/>
             <PrivateRoute path="/board/new" exact component={NewBoard} authed={authed} monkeyButt="i love monkeys"/>
---
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MyNavbar extends React.Component {
+  render() {
+    return(
+      <div className="MyNavbar">
+        <h1>IMA NAVBAR</h1>
+      </div>
+    );
+  }
+}
+
+export default MyNavbar;
