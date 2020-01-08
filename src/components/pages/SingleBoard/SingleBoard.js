7ae18ed116506f10f4edef776ba8150449a82b99
Zoe Ames <zoeames@gmail.com>
2020-01-07 20:24:59 -0600

    single board view displays

--- src/components/pages/SingleBoard/SingleBoard.js
+++ src/components/pages/SingleBoard/SingleBoard.js
@@ -1,14 +1,42 @@
 import React from 'react';
 
 import './SingleBoard.scss';
+import boardData from '../../../helpers/data/boardData';
+import pinData from '../../../helpers/data/pinData';
+
+import Pin from '../../shared/Pin/Pin';
 
 class SingleBoard extends React.Component {
-  render() {
+  state = {
+    board: {},
+    pins: [],
+  }
+
+  getPinData = (boardId) => {
+    pinData.getPinsByBoardId(boardId)
+    .then((pins) => this.setState({ pins }))
+    .catch((err) => console.error('error in get pins', err));
+  }
+
+  componentDidMount() {
     const { boardId } =  this.props.match.params;
+    boardData.getSingleBoard(boardId)
+      .then((response) => {
+        this.setState({ board: response.data});
+        this.getPinData(boardId);
+      })
+      .catch((err) => console.error('error in get single board', err));
+  }
+
+  render() {
+    const { board } = this.state;
     return (
       <div className="SingleBoard">
-        <h1>Single Board</h1>
-        <h2>Current Board Id is {boardId}</h2>
+        <h1>{board.name}</h1>
+        <p>{board.description}</p>
+        <div className="pins d-flex flex-wrap">
+          { this.state.pins.map((pin) => <Pin key={pin.id} pin={pin}/>) }
+        </div>
       </div>
     );
   }
--- src/components/shared/Board/Board.js
+++ src/components/shared/Board/Board.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import boardShape from '../../../helpers/propz/boardShape';
 
@@ -17,6 +18,7 @@ class Board extends React.Component {
           <div className="card-body">
             <h5 className="card-title">{board.name}</h5>
             <p className="card-text">{board.description}</p>
+            <Link className="btn btn-primary" to={`/board/${board.id}`}>View Board</Link>
           </div>
         </div>
       </div>
