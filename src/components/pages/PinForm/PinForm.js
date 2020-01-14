fb38c4ec3748e7268800c355a4371cc668805337
Zoe Ames <zoeames@gmail.com>
2020-01-13 19:23:51 -0600

    added pinform route and component

---
+++ src/components/pages/PinForm/PinForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './PinForm.scss'
+
+class PinForm extends React.Component {
+  render () {
+    return (
+      <form className="PinForm">
+        helpp
+      </form>
+    );
+  }
+}
+
+export default PinForm;
